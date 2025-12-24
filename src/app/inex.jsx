df4b701901770c695181e5d701d4da9ef5404871
Sema Nur Ata <atasemanur@icloud.com>
2025-12-25 00:01:27 +0300

    fixed app directory

---
+++ src/app/inex.jsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import LetterGlitch from './LetterGlitch';
+
+<LetterGlitch
+  glitchSpeed={50}
+  centerVignette={true}
+  outerVignette={false}
+  smooth={true}
+/>
